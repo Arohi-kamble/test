f6555a0a1610e0ff2c87c112db99b5cd7e4fa025
Arohi Kamble <arohi.kamble@dharamcs.com>
2025-08-04 17:57:36 +0530

    Added amplify-output.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState, FormEvent } from "react";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-// import outputs from "@/amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 
-// Amplify.configure(outputs);
-// const client = generateClient<Schema>();
+Amplify.configure(outputs);
+const client = generateClient<Schema>();
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -14,25 +14,25 @@ export default function Home() {
   const [departments, setDepartments] = useState<Schema["Department"]["type"][]>([]);
   const [selectedDept, setSelectedDept] = useState("");
 
-  // useEffect(() => {
-  //   // Fetch Departments (for dropdown)
-  //   const fetchDepartments = async () => {
-  //     const result = await client.models.Department.list();
-  //     setDepartments(result.data);
-  //   };
-  //   fetchDepartments();
-  // }, []);
+  useEffect(() => {
+    // Fetch Departments (for dropdown)
+    const fetchDepartments = async () => {
+      const result = await client.models.Department.list();
+      setDepartments(result.data);
+    };
+    fetchDepartments();
+  }, []);
 
   const handleRegister = async (e: FormEvent) => {
     e.preventDefault();
     if (!selectedDept) return alert("Please select a department");
 
     try {
-      // await client.models.User.create({
-      //   name,
-      //   email,
-      //   departmentID: selectedDept, // Foreign key
-      // });
+      await client.models.User.create({
+        name,
+        email,
+        departmentID: selectedDept, // Foreign key
+      });
       alert("User registered successfully!");
       setName("");
       setEmail("");
